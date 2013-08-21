0d53b9d6212967652089a57f49e2a87ddf70b0c7
Thomas Smyth <tom@tomsmyth.ca>
2013-08-21 10:54:27 -0400

    fixed engine file to work with rake

--- lib/diaedu/engine.rb
+++ lib/diaedu/engine.rb
@@ -1,15 +1,15 @@
 require 'diaedu/plugin'
 
 module Diaedu
-  class Engine < Rails::Engine
+  class Engine < ::Rails::Engine
 
     engine_name 'diaedu'
     isolate_namespace Diaedu
 
     initializer "diaedu.configure_rails_initialization" do |app|
 
-      app.config.after_initialize do 
-        DiscoursePluginRegistry.setup(Diaedu::Plugin)
+      app.config.after_initialize do
+      	DiscoursePluginRegistry.setup(Diaedu::Plugin) if Object.const_defined?('DiscoursePluginRegistry')
       end
     end
 
