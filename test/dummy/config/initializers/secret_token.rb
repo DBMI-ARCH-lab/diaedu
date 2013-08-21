bcc301ef2b4481b3726dfa062b9854363b408751
Thomas Smyth <tom@tomsmyth.ca>
2013-08-21 10:53:38 -0400

    added dummy test app and fixed Rakefile

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'fa18711ce425af1e66ad44066b85a313af4df75a78a75b07c46663004cdd66756581a6e5917516db9701e42749af15dbd83c30bf48e712db34b56a076a98dc0e'
