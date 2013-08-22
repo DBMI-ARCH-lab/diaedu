94e7f9c1d41b2959f5bad2cb3f85a45d7ceca27d
Thomas Smyth <tom@tomsmyth.ca>
2013-08-22 10:31:47 -0400

    styled glyprob index

--- app/models/diaedu/glyprob.rb
+++ app/models/diaedu/glyprob.rb
@@ -3,5 +3,10 @@ module Diaedu
     belongs_to(:event, :class_name => 'Diaedu::Event')
 
     scope(:by_event_name, includes(:event).order('diaedu_events.name, evaluation'))
+
+    def as_json(options = {})
+      # spoof the likes and comments attribs for now
+      super(options).merge(:likes => rand(30), :comments => rand(15))
+    end
   end
 end
