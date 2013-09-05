2b8109df4401481a0770aff4398fbc212d07c686
Thomas Smyth <tom@tomsmyth.ca>
2013-09-05 09:55:41 -0400

    made filter change do transition

--- app/assets/javascripts/diaedu/controllers/kb_filter_set_controller.js
+++ app/assets/javascripts/diaedu/controllers/kb_filter_set_controller.js
@@ -1,7 +1,19 @@
 Discourse.KbFilterSetController = Ember.ObjectController.extend({
+  needs: 'kb_obj',
+
   // handles changes to the filter set
   filterChanged: function() {
+
     // serialize the current filter set
-    var filter_params = this.get('model').serialize();
+    var new_filter_params = this.get('model').serialize();
+
+    // get current data type from the kb_obj controller
+    var data_type = this.get('controllers.kb_obj.content');
+
+    // build new obj page shell (objs to be fetched by obj page route)
+    var obj_page = Discourse.KbObjPage.create({page_id: 1, filter_params: new_filter_params})
+
+    // transition to new filter results
+    this.transitionToRoute('kb_obj.page', data_type, obj_page);
   }
 });
--- app/assets/javascripts/diaedu/models/kb_filter_set.js
+++ app/assets/javascripts/diaedu/models/kb_filter_set.js
@@ -2,8 +2,10 @@
 // knows how to fetch data for a set of filter blocks from the backend, given filter types.
 // we do the fetch in one go to save on overhead.
 Discourse.KbFilterSet = Discourse.Model.extend({
+  // the array of blocks
   blocks: null,
 
+  // converts the current state of the filter set into a string for the url (e.g. glyprobs-1,2,3-tags-4,5)
   serialize: function() {
     var chunks = [];
     this.get('blocks').forEach(function(block){
@@ -16,9 +18,12 @@ Discourse.KbFilterSet = Discourse.Model.extend({
   }
 });
 
+// class methods
 Discourse.KbFilterSet.reopenClass({
+  
   // generates a FilterSet containing a set of FilterBlock objects based on filterTypes and filterParams
   generate: function(filterTypes, filterParams) {
+    
     // call the backend asking for filter options matching the given filterTypes and filterParams
     return Discourse.ajax("/kb/filter-options", {data: {filter_types: filterTypes, filter_params: filterParams}}).then(function (data) {
       
