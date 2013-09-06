b8e1ed699b9ddbcdb014d4c20433efcafb83901a
Thomas Smyth <tom@tomsmyth.ca>
2013-09-06 10:30:08 -0400

    moved trigger controller to more generic kb_objs controller

--- app/controllers/diaedu/kb_objs_controller.rb
+++ app/controllers/diaedu/kb_objs_controller.rb
@@ -1,7 +1,7 @@
 require_dependency "diaedu/application_controller"
 
 module Diaedu
-  class TriggersController < ::ApplicationController
+  class KbObjsController < ::ApplicationController
     PER_PAGE = 10
 
     before_filter(:parse_filter_params, :only => :index)
@@ -10,12 +10,14 @@ module Diaedu
       page = params[:page].to_i || 1
 
       # sleep for a second in dev mode to test loading indicators
-      sleep(0.25) if Rails.env == 'development'
+      #sleep(0.25) if Rails.env == 'development'
       
+      klass = "Diaedu::#{params[:data_type].singularize.classify}".constantize
+
       render(:json => {
-        :objs => Diaedu::Trigger.filter_with(@filter).includes(:tags).offset((page - 1) * PER_PAGE).limit(PER_PAGE).all.as_json(:include => :tags),
+        :objs => klass.filter_with(@filter).includes(:tags).offset((page - 1) * PER_PAGE).limit(PER_PAGE).all.as_json(:include => :tags),
         :per_page => PER_PAGE,
-        :total_count => Diaedu::Trigger.filter_with(@filter).count
+        :total_count => klass.filter_with(@filter).count
       })
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Diaedu::Engine.routes.draw do
   root(:to => 'home#index')
+  
   resources(:glyprobs, :path => 'glycemic-problems')
   match('/glycemic-problems/page/:page' => 'glyprobs#index')
   match('/glycemic-problems/:filter_params/page/:page' => 'glyprobs#index')
+  
   resources(:triggers, :path => 'triggers')
   match('/triggers/page/:page' => 'triggers#index')
   match('/triggers/:filter_params/page/:page' => 'triggers#index')
-  resources(:goals, :path => 'goals')
-  match('/goals/page/:page' => 'goals#index')
-  match('/goals/:filter_params/page/:page' => 'goals#index')
+  
+  match('/goals' => 'kb_objs#index', :data_type => 'goals')
+  match('/goals/page/:page' => 'kb_objs#index', :data_type => 'goals')
+  match('/goals/:filter_params/page/:page' => 'kb_objs#index', :data_type => 'goals')
+
   match('/filter-options' => 'filter_options#fetch')
 end
