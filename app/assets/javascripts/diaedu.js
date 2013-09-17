337ea2048fc1f88f24f05687f078db341bc0ee37
Thomas Smyth <tom@tomsmyth.ca>
2013-09-17 18:15:28 -0400

    got basic autocomplete working for event box

--- app/assets/javascripts/diaedu.js
+++ app/assets/javascripts/diaedu.js
@@ -1,4 +1,4 @@
-//= require jquery.tokeninput-1.6.1-tns.js
+//= require_tree ./vendor
 //= require_tree ./diaedu/core-ext
 //= require ./diaedu/models/kb_obj.js
 //= require_tree ./diaedu
--- app/assets/javascripts/diaedu/views/kb_event_input_view.js
+++ app/assets/javascripts/diaedu/views/kb_event_input_view.js
@@ -3,6 +3,6 @@ Discourse.KbEventInputView = Discourse.View.extend({
   templateName: 'diaedu/templates/kb_glyprobs/event_input',
 
   didInsertElement: function() {
-
+    this.$('input').autocomplete2({source: ["alpha", "bravo"]});
   }
 });
