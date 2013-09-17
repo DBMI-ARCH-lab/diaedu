347eabe1cff99e9a6d381024efba9d94de6fc85e
Thomas Smyth <tom@tomsmyth.ca>
2013-09-17 09:04:13 -0400

    added token input control

--- app/assets/javascripts/diaedu/views/kb_tag_edit_view.js
+++ app/assets/javascripts/diaedu/views/kb_tag_edit_view.js
@@ -1,3 +1,17 @@
 Discourse.KbTagEditView = Discourse.View.extend({
   templateName: 'diaedu/templates/tag_edit',
+
+  didInsertElement: function() {
+    this.$('input').tokenInput('/kb/', {
+      hintText: I18n.t('kb.tags.type_to_add_new'),
+      noResultsText: I18n.t('kb.tags.none_found'),
+      searchingText: I18n.t('kb.tags.searching'),
+//      resultsFormatter: self.format_token_result,
+      preventDuplicates: true,
+      tokenValue: 'name'
+//      onResult: function(results){ return self.process_token_results(results); },
+//      onAdd: function(item){ return self.token_added(item); },
+//      onEnter: function(){ self.add_options(); }
+    });
+  }
 });
