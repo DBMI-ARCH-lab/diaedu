8f65851808e31334d73048d21d87e80b58fb2387
Thomas Smyth <tom@tomsmyth.ca>
2013-09-17 10:07:34 -0400

    refined tag control

--- app/assets/javascripts/diaedu/views/kb_tag_edit_view.js
+++ app/assets/javascripts/diaedu/views/kb_tag_edit_view.js
@@ -6,32 +6,9 @@ Discourse.KbTagEditView = Discourse.View.extend({
       hintText: I18n.t('kb.tags.type_to_add_new'),
       noResultsText: I18n.t('kb.tags.none_found'),
       searchingText: I18n.t('kb.tags.searching'),
-    //   resultsFormatter: this.formatTokenResult,
-    //   preventDuplicates: true,
-    //   tokenValue: 'name'
+      tokenValue: 'name',
+      preventDuplicates: true,
       zindex: 9999
-//      onResult: function(results){ return self.process_token_results(results); },
-//    onAdd: function(item){ return self.token_added(item); },
-//    onEnter: function(){ self.add_options(); }
     });
-  },
-
-  // returns the html to insert in the token input result list
-  formatTokenResult: function(item) {
-    console.log('FORMATTING');
-    // var details, css = "details";
-    // // if this is the new placeholder, add a string about that
-    // if (item.id == null) {
-    //   details = I18n.t('option_set.create_new');
-    //   css = "details create_new"
-    // // otherwise if no option sets were returned, use the none string
-    // } else if (item.set_names == '')
-    //   details = '[' + I18n.t('common.none') + ']'
-    // // otherwise just use item.sets verbatim
-    // else
-    //   details = item.set_names;
-    
-    //return '<li>' + item.name + '<div class="'+ css + '">' + details + '</div></li>';
-    return '<li>foo</li>';
   }
 });
--- app/models/diaedu/tag.rb
+++ app/models/diaedu/tag.rb
@@ -8,34 +8,29 @@ module Diaedu
     # returns an array of tags matching the given textual query
     # based on code from ELMO (https://github.com/thecartercenter/elmo)
     def self.suggestions(query)
+      query.downcase!
+      
       matches = where("name LIKE ?", "#{query}%").all
 
-      # scan matches
+      # scan matches for exact match
       exact_match = false
-      #matches.each_with_index do |m, i|
-        # set a flag if there is an exact match
-        #exact_match = true if 
+      matches.each_with_index do |t, i|
+        # if we find an exact match
+        if t.name == query
+          # move it to front of array and set flag
+          matches.unshift(matches.delete_at(i))
+          exact_match = true
+          break
+        end
+      end
 
-
-      # for i in 0...options.size
-      #   # if we have a a partial match
-      #   if options[i].name && options[i].name =~ /#{query}/i
-      #     # if also an exact match, set a flag and put it at the top
-      #     if options[i].name =~ /^#{query}$/i
-      #       matches.insert(0, options[i])
-      #       exact_match = true
-      #     # otherwise just insert at the end
-      #     else
-      #       matches << options[i]
-      #     end
-      #   end
-      # end
-      
       # trim results to max size (couldn't do this earlier b/c had to search whole list for exact match)
       matches = matches[0...MAX_SUGGESTIONS]
       
-      # if there was no exact match, we append a 'new option' placeholder
-      #matches << new(:name => query) unless exact_match
+      # if there was no exact match, we append a 'new' placeholder
+      matches << new(:name => query) unless exact_match
+
+      matches
     end
 
 
