321897871e10624451ddb505b5999bad29f75f5a
Thomas Smyth <tom@tomsmyth.ca>
2013-09-17 16:05:05 -0400

    moved event box into own view

---
+++ app/assets/javascripts/diaedu/views/kb_event_input_view.js
@@ -0,0 +1,8 @@
+// models a box for inputting an event, with autocomplete
+Discourse.KbEventInputView = Discourse.View.extend({
+  templateName: 'diaedu/templates/kb_glyprobs/event_input',
+
+  didInsertElement: function() {
+
+  }
+});
