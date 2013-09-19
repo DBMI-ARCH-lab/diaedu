5555eab001fcbf887730e41c28856c436a0d2193
Thomas Smyth <tom@tomsmyth.ca>
2013-09-19 11:34:45 -0400

    added boldifying matching portions of event in autocomplete

--- app/assets/javascripts/diaedu/views/kb_event_input_view.js
+++ app/assets/javascripts/diaedu/views/kb_event_input_view.js
@@ -2,7 +2,19 @@
 Discourse.KbEventInputView = Discourse.View.extend({
   templateName: 'diaedu/templates/kb_glyprobs/event_input',
 
-  didInsertElement: function() {
-    this.$('input').autocomplete2({source: '/kb/events/suggest'});
+  didInsertElement: function() { var self = this;
+    this.$('input').autocomplete2({source: '/kb/events/suggest'})
+
+    // do custom render to highlight matching portion
+    .data( "ui-autocomplete2" )._renderItem = function( ul, item ) {
+
+      // replace all occurrences of val (case insensitive) with boldified version
+      var val = self.$('input').val();
+      var lbl = item.label.replace(new RegExp(val, 'gi'), function(m) { return '<strong>' + m + '</strong>'; });
+
+      // build and append <li>
+      return $('<li>').append('<a>' + lbl + '</a>').appendTo(ul);
+    };
   }
+  
 });
