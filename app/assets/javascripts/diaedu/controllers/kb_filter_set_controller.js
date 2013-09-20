ea9282bb28dbfb2ee048382dec248dc4131cd3e4
Thomas Smyth <tom@tomsmyth.ca>
2013-09-20 15:25:31 -0400

    refactored kb filter set controller

--- app/assets/javascripts/diaedu/controllers/kb_filter_set_controller.js
+++ app/assets/javascripts/diaedu/controllers/kb_filter_set_controller.js
@@ -1,19 +1,19 @@
 Discourse.KbFilterSetController = Ember.ObjectController.extend({
-  needs: 'kb_obj',
+  needs: 'kbObj',
 
   // handles changes to the filter set
   filterChanged: function() {
 
     // serialize the current filter set
-    var new_filter_params = this.get('model').serialize();
+    var newFilterParams = this.get('model').serialize();
 
     // get current data type from the kb_obj controller
-    var data_type = this.get('controllers.kb_obj.content');
+    var dataType = this.get('controllers.kbObj.model');
 
     // build new obj page shell (objs to be fetched by obj page route)
-    var obj_page = Discourse.KbObjPage.create({page_id: 1, filter_params: new_filter_params})
+    var objPage = Discourse.KbObjPage.create({page_id: 1, filter_params: newFilterParams})
 
     // transition to new filter results
-    this.transitionToRoute('kb_obj_page.index', data_type, obj_page);
+    this.transitionToRoute('kb_obj_page.index', dataType, objPage);
   }
 });
