1bd2879f68591ddca33905d772272afa453ccb85
Thomas Smyth <tom@tomsmyth.ca>
2013-09-23 09:45:23 -0400

    added migration to create kb bot user

---
+++ db/migrate/20130923132604_create_kb_user.rb
@@ -0,0 +1,15 @@
+class CreateKbUser < ActiveRecord::Migration
+  def up
+    # creates a dummy user that automated kb actions will be associated with
+    User.create!(
+      :username => 'kbbot', 
+      :name => 'Knowledge Base Robot',
+      :email => 'diaedu-kb-bot@sassafrastech.com',
+      :active => true,
+      :username_lower => 'kbbot'
+    )
+  end
+
+  def down
+  end
+end
