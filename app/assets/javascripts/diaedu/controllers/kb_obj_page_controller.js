40d209de1c835aac18a864f27ee9d29c0e9f298c
Thomas Smyth <tom@tomsmyth.ca>
2013-09-24 10:09:09 -0400

    made pagination link work; refactored things to remove wrong datatype property

--- app/assets/javascripts/diaedu/controllers/kb_obj_page_controller.js
+++ app/assets/javascripts/diaedu/controllers/kb_obj_page_controller.js
@@ -1,15 +1,19 @@
 Discourse.KbObjPageController = Discourse.ObjectController.extend({
   needs: ["application", "kbObj"],
 
-  dataType: function() {
-    return this.get('controllers.kbObj.model');
-  }.property('controllers.kbObj'),
-
   showMode: function() {
     return this.get('controllers.application.currentPath') == 'kb_obj.show';
   }.property('controllers.application.currentPath'),
 
-  changePage: function(newPage) {
+  // loads the specified page into the model
+  changePage: function(newPage) { var self = this;
+    // start fetch and get promise
+    Discourse.KbObjPage.find(this.get('model.data_type'), newPage, this.get('model.filter_params'))
+    .done(function(loaded){
+      self.set('model', loaded);
     
+    }).fail(function(resp){
+      console.log("FETCH ERROR:", resp)
+    });
   }
 });
