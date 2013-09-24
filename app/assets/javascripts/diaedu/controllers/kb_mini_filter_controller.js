cc2700fa3faf45b161d34d56b08e28de0caa7bda
Thomas Smyth <tom@tomsmyth.ca>
2013-09-24 12:21:56 -0400

    added mini filter classes

---
+++ app/assets/javascripts/diaedu/controllers/kb_mini_filter_controller.js
@@ -0,0 +1,10 @@
+// a mini filter is a small row of links that can be used to filter one at a time
+// as opposed to a full filter set with blocks and checkboxes
+// associated with a single filter block model
+Discourse.KbMiniFilterController = Discourse.ObjectController.extend({
+
+  // handles changes to the filter set
+  filterChanged: function() {
+
+  }
+});
---
+++ app/assets/javascripts/diaedu/views/kb_mini_filter_view.js
@@ -0,0 +1,8 @@
+Discourse.KbMiniFilterView = Discourse.View.extend({
+  templateName: 'diaedu/templates/kb_objs/mini_filter',
+
+  // handle clicks on links
+  click: function(e) {
+
+  }
+});
