7d6f90bf10ff9cb7028c34995a09d1ce145fa03e
Thomas Smyth <tom@tomsmyth.ca>
2013-12-20 16:00:53 -0500

    added hook in view to fetch object and show button if appropriate

--- app/assets/javascripts/diaedu/views/kb_link_to_kb_button_view.js
+++ app/assets/javascripts/diaedu/views/kb_link_to_kb_button_view.js
@@ -1,13 +1,27 @@
 Discourse.KbLinkToKbButtonView = Discourse.ButtonView.extend({
   text: I18n.t('diaedu.view_in_kb'),
   title: I18n.t('diaedu.view_in_kb_title'),
+  hidden: true,
 
   /* initially hidden until we can check if this is a kb related post */
-  classNames: ['hidden'],
+  classNameBindings: ['hidden'],
 
   click: function(topic) { var self = this;
-    var kb_obj = Discourse.KbGlyprob.create({id: 50});
-    self.get('controller').transitionToRoute('kb_obj.show_with_breadcrumb', kb_obj.get('dataType'), kb_obj);
+    // transition to the linked object's show action
+    var obj = self.get('controller.kbObj');
+    self.get('controller').transitionToRoute('kb_obj.show_with_breadcrumb', obj.get('dataType'), obj);
+  },
+
+  didInsertElement: function() { var self = this;
+    // fetch the kb obj related to this topic (if available)
+    // I feel like this should really be done in the controller but couldn't figure out an appropriate thing to hook on to
+    Discourse.KbObj.findByTopicId(self.get('controller.controllers.topic.content.id')).then(function(obj){
+      // store in controller
+      self.set('controller.kbObj', obj);
+
+      // set hidden depending on if null
+      self.set('hidden', obj === null);
+    });
   },
 
   renderIcon: function(buffer) { var self = this;
