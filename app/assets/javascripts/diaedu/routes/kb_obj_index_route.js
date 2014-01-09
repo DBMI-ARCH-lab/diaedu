e8784c4e8b3aa47f05577e0b9b64b6e790987e5e
Thomas Smyth <tom@tomsmyth.ca>
2014-01-09 15:48:06 -0500

    fixed bug in index route

--- app/assets/javascripts/diaedu/routes/kb_obj_index_route.js
+++ app/assets/javascripts/diaedu/routes/kb_obj_index_route.js
@@ -1,6 +1,6 @@
 Discourse.KbObjIndexRoute = Discourse.Route.extend({
   redirect: function(model) {
     // transition to the first page, no filter
-    this.transitionTo('kb_obj.filtered_page', {page_id: 1, filter_params: 'all'});
+    this.transitionTo('kb_obj.filtered_page', Discourse.KbObjPage.create({page_id: 1, filter_params: 'all'}));
   }
 });
