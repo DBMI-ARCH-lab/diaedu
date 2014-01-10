b84475087bf00a36bff84d73bd9cbc8c7ed3359c
Thomas Smyth <tom@tomsmyth.ca>
2014-01-10 13:08:46 -0500

    changed kbHome route name to camel case

--- app/assets/javascripts/diaedu/router.js
+++ app/assets/javascripts/diaedu/router.js
@@ -1,6 +1,6 @@
 Discourse.Route.buildRoutes(function() {
   var router = this;
-  this.route('kb_home', {path: '/kb'});
+  this.route('kbHome', {path: '/kb'});
 
   this.resource('kb_obj', {path: '/kb/:data_type'}, function() {
     this.route('show', {path: '/:id'});
