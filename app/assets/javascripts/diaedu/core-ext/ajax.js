f1be841fd66545ad3b9507af83bfa6e90e7e192b
Thomas Smyth <tom@tomsmyth.ca>
2014-01-10 13:49:32 -0500

    added cleanAjax function to handle snake/camel case

---
+++ app/assets/javascripts/diaedu/core-ext/ajax.js
@@ -0,0 +1,32 @@
+// a wrapper for the Discourse.ajax function that converts any outgoing data keys into snake_case, and vice versa for incoming data
+Discourse.cleanAjax = function() {
+
+  var snakeToCamel = function(s) { return s.replace(/(\_[a-z])/g, function($1){return $1.toUpperCase().replace('_','');}); };
+  var camelToSnake = function(s) { return s.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();}); };
+
+  var transformHash = function(hash, transformer) {
+    var newHash = {};
+
+    for (var k in hash) {
+      // recurse
+      if (typeof hash[k] == 'object')
+        hash[k] = transformHash(hash[k], transformer);
+
+      // transform the keys
+      newHash[transformer(k)] = hash[k];
+    }
+
+    return newHash;
+  };
+
+  // transform outgoing to snake
+  if (arguments[1] && arguments[1].data) {
+    arguments[1].data = transformHash(arguments[1].data, camelToSnake);
+  }
+
+  // send ajax
+  return Discourse.ajax(arguments[0], arguments[1]).then(function(data){
+    // transform incoming to camel
+    return transformHash(data, snakeToCamel);
+  });
+};
