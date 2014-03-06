adf545dbe5199932599e3990918ef4ece7410f47
Thomas Smyth <tom@tomsmyth.ca>
2014-03-06 15:08:03 -0500

    added removal of old orphan items

--- app/controllers/diaedu/evidence_controller.rb
+++ app/controllers/diaedu/evidence_controller.rb
@@ -2,6 +2,9 @@ module Diaedu
   class EvidenceController < ::ApplicationController
 
     def create
+      # remove old orphan items
+      EvidenceItem.where(:kb_obj_id => nil).where('created_at < ?', 2.hours.ago).destroy_all
+
       # create new EvidenceItem and attach file
       @item = EvidenceItem.create!(params.require(:evidence_item).permit(:file).merge(:temporary => true))
 
