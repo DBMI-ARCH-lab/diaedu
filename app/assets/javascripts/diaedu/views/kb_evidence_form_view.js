cc808411207347434f6a356ab520fc7676e63a95
Thomas Smyth <tom@tomsmyth.ca>
2014-03-06 12:09:02 -0500

    removed unused file

--- app/assets/javascripts/diaedu/views/kb_evidence_form_view.js
+++
@@ -1,5 +0,0 @@
-// view for the whole evidence area and all evidence items
-Discourse.KbEvidenceFormView = Discourse.View.extend({
-  templateName: 'javascripts/diaedu/templates/evidence_form',
-  classNames: 'evidence-form'
-});
