895c61129311aa6e532e7cf7d10827308f2deffc
Thomas Smyth <tom@tomsmyth.ca>
2014-03-07 14:12:08 -0500

    added view/print button

--- app/assets/javascripts/diaedu/controllers/kb_obj_show_with_breadcrumb_controller.js
+++ app/assets/javascripts/diaedu/controllers/kb_obj_show_with_breadcrumb_controller.js
@@ -8,16 +8,15 @@ Discourse.KbObjShowWithBreadcrumbController = Discourse.ObjectController.extend(
   // KbFilterBlock object for tags, to be used in the minifilter
   tagFilterBlock: null,
 
+  // checks if the current user is allowed to post comments
+  canComment: function() {
+    return !!this.get('currentUser');
+  }.property('currentUser'),
+
   actions: {
     // likes the current KbObj by either creating a new topic for it or liking the existing one
     likeObj: function() { var self = this;
       self.get('model').like();
     }
-  },
-
-  // checks if the current user is allowed to post comments
-  canComment: function() {
-    return !!this.get('currentUser');
-  }.property('currentUser')
-
+  }
 });
